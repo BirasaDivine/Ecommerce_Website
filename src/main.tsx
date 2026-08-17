e79351f4edbf96552220a4b21586bcd83873d5af
Birasa Divine <b.divine@alustudent.com>
2026-08-17 17:11:33 +0200

    update to use context

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import ShopContextProvider from './context/ShopContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ShopContextProvider>
+        <App />
+      </ShopContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
