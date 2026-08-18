951a85112b87b5ba47d505c1f928a7094efc9f53
Birasa Divine <b.divine@alustudent.com>
2026-08-18 11:27:09 +0200

    refactor main.tsx to wrap ShopContextProvider with AuthProvider for improved authentication context

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import ShopContextProvider from './context/ShopContext.tsx'
+import AuthProvider from './context/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <ShopContextProvider>
-        <App />
-      </ShopContextProvider>
+      <AuthProvider>
+        <ShopContextProvider>
+          <App />
+        </ShopContextProvider>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
